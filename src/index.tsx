68ecb5c30347826432a0aeeb3f300c81a40936d2
Die <dlibonati@leafnoise.io>
2024-10-30 12:01:42 -0300

    patch: imports updated

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,10 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
+
 import CounterApp from "./App";
+
 import "./index.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <CounterApp />
-  </React.StrictMode>
-);
+root.render(<CounterApp />);
